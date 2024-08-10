8c0ee3e82f2e404d39df67c0efacba158afe319f
carlos hernandez <chernandezg37@miumg.edu.gt>
2024-08-10 01:32:46 -0600

    se subio la vista con correcciones

--- RentaVideo/src/Seguridad/Modelo/Conexion.java
+++ RentaVideo/src/Seguridad/Modelo/Conexion.java
@@ -20,7 +20,7 @@ public class Conexion {
 
 private static final String JDBC_URL = "jdbc:mysql://localhost/tienda_videos?useSSL=false&serverTimezone=UTC";    
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASS = "200118";
+    private static final String JDBC_PASS = "carlos04";
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
     }
--- RentaVideo/src/Vista/Vista_Administrador.java
+++ RentaVideo/src/Vista/Vista_Administrador.java
@@ -167,6 +167,11 @@ public class Vista_Administrador extends javax.swing.JFrame {
         jMenu6.add(jMenuItem1);
 
         jMenuItem3.setText("Registro de clientes ");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         jMenu6.add(jMenuItem3);
 
         jMenuItem2.setText("Bitácora");
@@ -360,6 +365,10 @@ public class Vista_Administrador extends javax.swing.JFrame {
         }
         
     }//GEN-LAST:event_jMenuItem9ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
